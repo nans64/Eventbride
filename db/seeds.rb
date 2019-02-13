654c1b9d547c9735dc57fddbde49981b714021fd
nans64 <nans.noel@gmail.com>
2019-02-14 00:45:28 +0100

    Add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,35 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-15.times do
-  user = User.create!(email: Faker::Internet.email, encrypted_password: SecureRandom.urlsafe_base64, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::SiliconValley.quote)
-  puts ""
-  puts "user #{user.first_name} #{user.last_name}"
-  sleep(0.05)
-  puts "#{user.description}"
-  puts ""
-  puts "~~~~~~~~~~~~~~~~~~~next~~~~~~~~~~~~~~~~>"
+15.times do |i|
+  user = User.create!(
+      first_name: Faker::Internet.username,
+      last_name: Faker::LeagueOfLegends.champion,
+      description: Faker::LeagueOfLegends.quote,
+      email: Faker::Internet.email,
+      password: Faker::Space.agency)
+  print '.'
 end
+puts '=== User DB created ==='
 
-10.times do
-  e=Event.create(start_date: DateTime.new(2019,rand(6..12),rand(1..28)), duration:[10,15,20,25,30,35,40,45,75,125].sample , title:Faker::ChuckNorris.fact , description:Faker::TvShows::HowIMetYourMother.quote , price: rand(1..1000) , location:Faker::WorldCup.city , user_id: User.last)
+10.times do |j|
+  j += 1
+  event = Event.create!(
+      user_id: User.all.sample.id,
+      title: Faker::Music.instrument + Faker::Music.instrument,
+      description: Faker::HitchhikersGuideToTheGalaxy.quote + Faker::MostInterestingManInTheWorld.quote,
+      start_date: Faker::Date.forward(rand(1..666)),
+      duration: j * 5,
+      price: rand(1..1000),
+      location: Faker::LeagueOfLegends.location)
+  print '.'
 end
+puts '=== Event DB created ==='
 
-
-10.times do
-  a=Attendance.create(stripe_customer_id: Faker::Code.ean, user: User.last, event: Event.last)
+30.times do |k|
+  attendance = Attendance.create!(
+      user_id: User.all.sample.id,
+      event_id: Event.all.sample.id,
+      stripe_customer_id: Faker::Business.credit_card_number)
+  print '.'
 end
-
